f2e68e3e2aa33652994ed1e5448cfda60b9d40fd
Vayam Jain <vayam.jain@gmail.com>
2020-12-21 02:53:16 +0530

    Added error types and messages

---
+++ app/utils/errors.js
@@ -0,0 +1,10 @@
+export const Errors = {
+  unauthorized: 'unauthorized',
+  forbidden: 'forbidden',
+  internal: 'internal'
+}
+
+export const Messages = {
+  internal: 'An error occurred while processing your request. Please try again later.',
+  emailAlreadyRegistered: 'Email already registered! Please login'
+}
