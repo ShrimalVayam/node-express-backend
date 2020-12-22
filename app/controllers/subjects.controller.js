d89470b81f41a1b6c0b711a0e7672f2891f16a3c
Vayam Jain <vayam.jain@gmail.com>
2020-12-22 21:22:46 +0530

    Added list all subjects service

--- app/controllers/subjects.controller.js
+++ app/controllers/subjects.controller.js
@@ -1,9 +1,15 @@
 import * as Responder from '../server/expressResponder'
 import createSubjectService from '../services/subjects/create'
+import listAllSubjectsService from '../services/subjects/listAll'
 
 const createSubjects = async (req, res) => {
   const result = await createSubjectService({ ...req.body, ...req.user })
   Responder.checker(res, result)
 }
 
-export { createSubjects }
+const listAllSubjects = async (req, res) => {
+  const result = await listAllSubjectsService()
+  Responder.checker(res, result)
+}
+
+export { createSubjects, listAllSubjects }
