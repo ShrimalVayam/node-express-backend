9254225b6273de59fe5a2e13c0cea558ac1c8a96
Vayam Jain <vayam.jain@gmail.com>
2020-12-22 21:08:27 +0530

    Added resource conflict

--- app/utils/errors.js
+++ app/utils/errors.js
@@ -3,12 +3,14 @@ export const Errors = {
   forbidden: 'forbidden',
   internal: 'internal',
   badRequest: 'badRequest',
-  notFound: 'notFound'
+  notFound: 'notFound',
+  conflict: 'conflict'
 }
 
 export const Messages = {
   internal: 'An error occurred while processing your request. Please try again later.',
   emailAlreadyRegistered: 'Email already registered! Please login',
   emailNotRegistered: 'Email is not registered',
-  incorrectPassword: 'The password entered is incorrect'
+  incorrectPassword: 'The password entered is incorrect',
+  resourceConflict: (resource) => `The ${resource} already exists`
 }
