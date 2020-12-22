cc960e423d6bd59b3d733b401d24c615fbfe103d
Vayam Jain <vayam.jain@gmail.com>
2020-12-22 21:10:40 +0530

    Added responder checker

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -5,11 +5,7 @@ import * as Responder from '../server/expressResponder'
 
 export const signup = async (req, res) => {
   const result = await signupService(req.body)
-  if (!result.error) {
-    Responder.success(res, result)
-  } else {
-    Responder.failed(res, result.error)
-  }
+  Responder.checker(res, result)
 }
 
 export const login = async (req, res) => {
