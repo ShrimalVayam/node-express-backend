30a9356ea7aa59e489d1bd768b6bc4a525a07a3b
Vayam Jain <vayam.jain@gmail.com>
2020-12-22 21:50:14 +0530

    Added optional check and length check message

--- app/validation/common/index.js
+++ app/validation/common/index.js
@@ -2,4 +2,12 @@ import { body } from 'express-validator'
 
 const bodyExists = (field, errorMessage = 'This field is required') => body(field).exists().withMessage(errorMessage)
 
-export { bodyExists }
+const bodyOptional = (field) => body(field).optional()
+
+const lengthMessage = (fieldName, min, max) => {
+  let message = `The ${fieldName} should be `
+  if (min) message = message + `a minumum of ${min} `
+  if (max) message = message + `and a maximum of ${max} `
+  return message + 'characters'
+}
+export { bodyExists, bodyOptional, lengthMessage }
