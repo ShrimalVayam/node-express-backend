c9c20d745caa5d262b0a7bfdece29fbc3569963b
Vayam Jain <vayam.jain@gmail.com>
2020-12-22 22:58:10 +0530

    Added subject update validations

--- app/validation/subjects/index.js
+++ app/validation/subjects/index.js
@@ -1,5 +1,15 @@
-import { bodyExists } from '../common'
+import { bodyExists, bodyOptional, lengthMessage, paramExists } from '../common'
+
+const { oneOf } = require('express-validator')
 
 export const createSubject = [
-  bodyExists('name').isLength({ max: 50 }).withMessage('The name is too long')
+  bodyExists('name').isLength({ min: 5, max: 50 }).withMessage(lengthMessage('name', 5, 50)),
+  bodyOptional('type').isLength({ min: 5, max: 15 }).withMessage(lengthMessage('type', 5, 15))
 ]
+
+export const updateSubject = [
+  paramExists('subjectId').isInt(),
+  oneOf([
+    bodyExists('name').isLength({ min: 5, max: 50 }).withMessage(lengthMessage('name', 5, 50)),
+    bodyExists('type').isLength({ min: 5, max: 15 }).withMessage(lengthMessage('type', 5, 15))
+  ])]
