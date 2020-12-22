4919587e7ac670ac8508c02fee3336334cf9fe11
Vayam Jain <vayam.jain@gmail.com>
2020-12-22 16:22:31 +0530

    Added other errors

--- app/utils/errors.js
+++ app/utils/errors.js
@@ -2,10 +2,13 @@ export const Errors = {
   unauthorized: 'unauthorized',
   forbidden: 'forbidden',
   internal: 'internal',
-  badRequest: 'badRequest'
+  badRequest: 'badRequest',
+  notFound: 'notFound'
 }
 
 export const Messages = {
   internal: 'An error occurred while processing your request. Please try again later.',
-  emailAlreadyRegistered: 'Email already registered! Please login'
+  emailAlreadyRegistered: 'Email already registered! Please login',
+  emailNotRegistered: 'Email is not registered',
+  incorrectPassword: 'The password entered is incorrect'
 }
